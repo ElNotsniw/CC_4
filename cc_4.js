eb737bff52487b0d91d5ee7f014ee8905582d891
ElNotsniw <winstonjosh010604@gmail.com>
2025-02-01 15:54:31 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -11,3 +11,16 @@ if (purchaseamount > 100) {
 }
 
 console.log(`Grand Total after applicable discount: $${finalamount}`);      // Final output after applicable discounts
+
+
+// Task 2 - Sales Report
+
+let sales = [50, 100, 150, 200, 250, 300]           // Original output to start calculating the total of these six values
+let totalsales = 0                                  // Have to declare totalsales at 0 to then add up all the values from sales
+
+for (let i = 0; i < sales.length; i++) {            // for loop to help add all the values together to get the totalsales
+    totalsales += sales[i];
+    
+}
+
+console.log(`Total Sales: $${totalsales}`)          // Final output after adding up all values
